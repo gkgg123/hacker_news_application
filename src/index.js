99d7fe7c2f90dd74867cbc55efe5baa7d050bc42
gkgg123 <gkggg123@gmail.com>
2021-10-20 21:33:01 +0900

    ADD : sub news rendering

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const content = document.createElement('div');
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
+window.addEventListener('hashchange', (event) => {
+  const id = location.hash.substr(1);
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.send();
+  const newsContent = JSON.parse(ajax.response);
+  const title = document.createElement('h1');
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
 
+})
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul')
-ul.innerHTML = newsFeed.map(item => `<li>${item.title}</li>`).join('');
+ul.innerHTML = newsFeed.map(item => `<li><a href="#${item.id}">${item.title} (${item.comments_count}))</a></li>`).join('');
 document.querySelector('#root').appendChild(ul);
+document.querySelector('#root').appendChild(content);
