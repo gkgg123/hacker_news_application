0d20ad2717851f2ccf403cf63c20e2f3a69e332a
gkgg123 <gkggg123@gmail.com>
2021-10-20 21:34:05 +0900

    MODIFY : duplicate code constantization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-const content = document.createElement('div');
+const rootElement = document.querySelector('#root');
+const contentElement = document.createElement('div');
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
 window.addEventListener('hashchange', (event) => {
@@ -11,11 +12,11 @@ window.addEventListener('hashchange', (event) => {
   const newsContent = JSON.parse(ajax.response);
   const title = document.createElement('h1');
   title.innerHTML = newsContent.title;
-  content.appendChild(title);
+  contentElement.appendChild(title);
 
 })
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul')
 ul.innerHTML = newsFeed.map(item => `<li><a href="#${item.id}">${item.title} (${item.comments_count}))</a></li>`).join('');
-document.querySelector('#root').appendChild(ul);
-document.querySelector('#root').appendChild(content);
+rootElement.appendChild(ul);
+rootElement.appendChild(contentElement);
